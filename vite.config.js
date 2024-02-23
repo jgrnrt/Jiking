c077e7616da0510b4046abdec3d690b5547183f0
Jeremy Grunert <gotteswillenwilles@gmail.com>
2024-02-23 17:43:13 +0100

    base angepasst

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Jiking/' : '/',
+  base: '/Jiking',
+  //base: process.env.NODE_ENV === 'production' ? '/Jiking/' : '/',
   plugins: [react()],
 })
